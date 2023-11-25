3d657d189947f0aa4d8ed37e93e8631fe27fef9c
jisnarose6y <119037155+jisnarose6y@users.noreply.github.com>
2023-11-25 16:56:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
-provider "aws"{
-    region = "us-east-1"
+#Terraform Deployment Providers
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
