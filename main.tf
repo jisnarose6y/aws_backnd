6eb538645e7964304e0f3d5e3b3e89e6d1c5e409
jisnarose6y <119037155+jisnarose6y@users.noreply.github.com>
2023-11-25 16:49:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
  terraform {
     backend "s3" {
-      bucket = "terraform-backend-keyshell"
+      bucket = "terraform112455"
      key = "terraform.tfstate"
       region = "us-east-1"
       dynamodb_table = "terraform"
