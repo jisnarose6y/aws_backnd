2725f882ff22a10f2441e9f5faa38cb9097d9e6d
jisnarose6y <119037155+jisnarose6y@users.noreply.github.com>
2023-11-25 17:00:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,18 +1,35 @@
+#VPC/Security Group Variables
+variable "vpc" {
+  description = "ID of VPC Used"
+  type        = string
+  default     = "vpc-0c5c8875f856c2cac"
+}
+
+variable "jenkins_security_name" {
+  description = "Security Group for EC2/Jenkins"
+  type        = string
+  default     = "jenkins-sg"
+}
+
+#EC2 Variables
+variable "ami" {
+  description = "Amazon Linux 2 AMI"
+  type        = string
+  default     = "ami-0dfcb1ef8550277af"
+}
+
 variable "instance_type" {
-  description = "ec2 instance type"
-  type = string
-  default = "t2.micro"
+  description = "Instance Type of EC2"
+  type        = string
+  default     = "t2.micro"
 }
 
-variable "public_ip" {
-  description = "Public IP value"
-  type = bool
-  default = true
-}
 
-variable "tags" {
-  type = map(string)
-  default = {
-    "Name" = "chess"
-  }
+#S3 Bucket Variables
+
+variable "s3_bucket_name" {
+  description = "Bucket for Jenkins Artifacts"
+  type        = string
+  default     = "terraform112455"
+  dynamodb_table = "terraform"
 }
